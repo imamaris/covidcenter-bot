81e915f1439f9ae987a3f140d3aeab72630fafd7
gazandi <gazandi@ruangguru.com>
2020-11-29 14:12:09 +0700

    feat: sync test and data

--- test/covid_utterance_test.js
+++ test/covid_utterance_test.js
@@ -5,22 +5,24 @@ const {
   queryWit
 } = require('../shared.js')
 
-const intentName = "sentiment_intent"
+const intentName = "covid_intent"
 const DOUBLETAB = '   ';
-const fileName = 'init-data/test.tsv'
+const fileName = 'init-data/datasets/covid_intent_test.tsv'
 const data = fs
   .readFileSync(fileName, 'utf-8')
   .split('\n')
   .map((row) => row.split(DOUBLETAB))
 
 describe('API test', function () {
-  describe('#sentiment_test()', function () {
+  describe('#covid_intent_test()', function () {
     it('same', function () {
-      const samples = data.map(([text, trait, value]) => {
+      const samples = data.map(([text, value, start, end]) => {
         queryWit(text).then((res) => {
           assert.equal(res.text, text);
           assert.equal(res.intents[0].name, intentName);
-          assert.equal(res.traits.sentiment[0].value, negatif);
+          assert.equal(res.entities["covid:covid"][0].value, value)
+          assert.equal(res.entities["covid:covid"][0].start, start)
+          assert.equal(res.entities["covid:covid"][0].end, end)
         })
       });
     });
